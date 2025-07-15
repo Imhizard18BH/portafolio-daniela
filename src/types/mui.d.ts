d7ac2ac41c7e220e3965385b2ef3df01b8a4eab9
Brahyam_Hurtado <brahyam@atomogaming.com>
2025-07-15 11:51:30 -0500

    feat: initial commit

---
+++ src/types/mui.d.ts
@@ -0,0 +1,24 @@
+declare module '@mui/material' {
+  export interface GridProps {
+    item?: boolean;
+    xs?: boolean | number;
+    sm?: boolean | number;
+    md?: boolean | number;
+    lg?: boolean | number;
+    xl?: boolean | number;
+  }
+  
+  export const Box: any;
+  export const Container: any;
+  export const Typography: any;
+  export const Grid: any;
+  export const Dialog: any;
+  export const IconButton: any;
+  export const Chip: any;
+  export const AppBar: any;
+  export const Toolbar: any;
+  export const Drawer: any;
+  export const Button: any;
+  export const useMediaQuery: any;
+  export const useTheme: any;
+}
